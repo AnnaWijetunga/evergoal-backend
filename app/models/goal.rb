7661313dffcba6c7ff1c39ae18df31c24a7c8a29
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 13:16:40 -0400

    Add relationships between goals and objectives as well as validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,7 @@
 class Goal < ApplicationRecord
+    has_many :objectives, dependent: :destroy
+    validates :aim, :strategy, presence: true
 end
+
+# If you destroy a record that has_many associated records, by default, those associated records will be left in the database. 
+# If you want associated records to be removed when their parent is removed, add the dependent: :destroy option to the has_many association in the model class.
--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,2 +1,4 @@
 class Objective < ApplicationRecord
+    belongs_to :goal
+    validates :description, presence: true
 end
