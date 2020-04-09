9c63f6c19db769809540215e19a9a20e42c438ef
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 13:25:57 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+goal1 = Goal.create(aim:"Run", strategy: "Run 6 days a week to build up to a marathon.")
+goal2 = Goal.create(aim:"Cook", strategy: "Menu plan and create weekly meals.")
+
+objective1 = Objective.create(description: "Track my runs", goal_id: 1, completed: 0)
+objective2 = Objective.create(description: "Increase mileage weekly", goal_id: 1, completed: 0)
+objective3 = Objective.create(description: "Buy new shoes", goal_id: 1, completed: 0)
+
+objective4 = Objective.create(description: "Set aside a day for weekly planning", goal_id: 2, completed: 0)
+objective5 = Objective.create(description: "Set aside time to write down ingredients", goal_id: 2, completed: 0)
