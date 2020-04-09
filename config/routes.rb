dccfe2fec050420a68cedf1ed56390376a250cb0
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 14:08:35 -0400

    Add scope and nesting to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  resources :objectives
-  resources :goals
+
+  scope :api do
+    resources :goals do
+      resources :objectives
+    end
+  end
+
 end
+
+# original routes and here below
+# resources :objectives
+# resources :goals
+
+# let's talk scope
+# when using scope without any options and only a scope name, it will just change the resources path.
+# my routes are now scoped (api) and nested (objectives within goals): 
+# /api/goals/:goal_id/objectives(.:format) -- goal_objectives_path 
+
+# to see all routes: http://localhost:3000/rails/info/routes (so my brain can stop trying to remember)
