92fa7e39c36ed583df548052f47b7bda9b9a0a69
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 15:31:46 -0400

    Add custom routes for toggle action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/api/goals/:id/toggle', to: 'goals#toggle'
+  resources :objectives, only: [:destroy]
+
+  patch '/api/objectives/:id', to: 'objectives#toggle'
+
 end
 
 # original routes and here below
