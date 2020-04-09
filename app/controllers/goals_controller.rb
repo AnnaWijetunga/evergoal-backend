e6114678866ebfdf21151a5d07f2eca19b4ae709
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 15:03:31 -0400

    Create route/action templates for goals and objectives

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,32 @@
 class GoalsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def destroy
+
+    end
+
+    def toggle
+
+    end
+
+    private
+
+    def goal_params
+
+    end
+
+    def set_goal
+        
+    end
 end
--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,2 +1,36 @@
 class ObjectivesController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def toggle
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def objective_params
+
+    end
+
+    def set_goal
+
+    end
+
+    def set_objective
+        
+    end
 end
