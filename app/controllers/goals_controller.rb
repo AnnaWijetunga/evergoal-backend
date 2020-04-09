7a6f8e286e0517fe2a8bbc8b3d62159f915f4730
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 15:20:25 -0400

    Create actions for goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,32 +1,37 @@
 class GoalsController < ApplicationController
+    before_action :set_goal, only: [:show, :destroy, :toggle]
 
     def index
-
+        @goals = Goal.all 
+        render json: @goals
     end
 
     def show
-
+        render json: @goal
     end
 
     def create
-
+        @goal = Goal.create(goal_params)
+        render json: @goal
     end
 
     def destroy
-
+        @goal.destroy 
+        render json: @goal
     end
 
     def toggle
-
+        @goal.toggle!
+        render json: @goal
     end
 
     private
 
     def goal_params
-
+        params.require(:goal).permit(:aim, :strategy)
     end
 
     def set_goal
-        
+        @goal = Goal.find(params[:id])
     end
 end
