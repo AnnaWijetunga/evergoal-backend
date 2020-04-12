0ae77b331db76b2f49597c3e47cd2e5545b65da2
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-12 13:28:09 -0400

    Replaced SQLite3 with PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_09_164946) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "goals", force: :cascade do |t|
     t.string "aim"
     t.string "strategy"
