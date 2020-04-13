b629a5b119c001e0ab9e9cedec5c40fc34f9d216
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-13 08:01:23 -0400

    Update serializers to clarify purpose - notes

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,3 +1,9 @@
+# which parts of our goal do we think will be imoportant to access on our front end? id, aim, strategy, and completed
+
+# below we add our attributes we want to access for our goal
+
+# then we need to ensure objectives come through, nested under goals - we add the has_many relationship
+
 class GoalSerializer < ActiveModel::Serializer
   attributes :id, :aim, :strategy, :completed
   has_many :objectives 
--- app/serializers/objective_serializer.rb
+++ app/serializers/objective_serializer.rb
@@ -1,3 +1,7 @@
+# below we add our attributes we want to access for our objective
+
+# then we need to ensure goal comes through, as a parent to objectives - we add the belongs_to relationship
+
 class ObjectiveSerializer < ActiveModel::Serializer
   attributes :id, :goal_id, :description, :completed
   belongs_to :goal
