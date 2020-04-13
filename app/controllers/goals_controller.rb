3d1fdc5db9fcdf1f1c0b19cdafbee27e2a10ef1c
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-13 07:51:56 -0400

    Update set_goal and our before_action - clarify necessity and order

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,13 +2,14 @@ class GoalsController < ApplicationController
     # before any action, our set_goal method will run
     before_action :set_goal, only: [:show, :destroy, :toggle]
 
+    # show all goals and render as JSON
     def index
         @goals = Goal.all 
         render json: @goals
     end
 
     def show
-        # @goal = Goal.find(params[:id])
+        # @goal = Goal.find(params[:id]) -- not needed due to before_action of :set_goal
         render json: @goal
     end
 
@@ -46,10 +47,12 @@ class GoalsController < ApplicationController
 
     private
 
+    # our params require a goal and permit the attributes of aim and strategy
     def goal_params
         params.require(:goal).permit(:aim, :strategy)
     end
 
+    # we want an index of all transactions associated with a specific account - this will run before any of our routes above
     def set_goal
         @goal = Goal.find(params[:id])
     end
