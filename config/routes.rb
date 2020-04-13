5e44e9e108879de777d4e288bccc599fed9e1226
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-13 07:36:34 -0400

    Update notes on scope, nested routes, custom routes and route info

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,32 @@
+# the routes here are the routes we’ll be making fetch requests to from our front end
+
 Rails.application.routes.draw do
 
+  # routes are scoped with api, and objectives are nested under goals so objectives can only be reached through goals
   scope :api do
     resources :goals do
       resources :objectives
     end
   end
 
+  # custom route for the toggle function
   get '/api/goals/:id/toggle', to: 'goals#toggle'
   resources :objectives, only: [:destroy]
 
+  # custom route for the toggle function
   patch '/api/objectives/:id', to: 'objectives#toggle'
 
+  # fallback route - not certain it's needed but it seems like a good idea
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 
 end
 
-# original routes and here below
-# resources :objectives
-# resources :goals
+# SCOPE
+# using scope without any options (only a scope name), it will change only the resources path
+# my routes are now scoped (api - so it's clear we're fetching to an api) and nested (objectives within goals): 
+# example: /api/goals/:goal_id/objectives(.:format) -- goal_objectives_path 
 
-# let's talk scope
-# when using scope without any options and only a scope name, it will just change the resources path.
-# my routes are now scoped (api) and nested (objectives within goals): 
-# /api/goals/:goal_id/objectives(.:format) -- goal_objectives_path 
-
-# to see all routes: http://localhost:3000/rails/info/routes (so my brain can stop trying to remember)
+# ALL ROUTES
+# all routes: http://localhost:3000/rails/info/routes
